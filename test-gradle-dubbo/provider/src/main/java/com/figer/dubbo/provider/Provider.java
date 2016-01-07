d8b355006fc65a7c836b0807fab9b3c52392a765
peng.zhang <peng.zhang@travelzen.com>
2016-01-07 15:18:50 +0800

    test gradle : define and use libraries.gradle

--- test-gradle-dubbo/provider/src/main/java/com/figer/dubbo/provider/Provider.java
+++ test-gradle-dubbo/provider/src/main/java/com/figer/dubbo/provider/Provider.java
@@ -2,10 +2,16 @@ package com.figer.dubbo.provider;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Provider {
+	private static final Logger logger = LoggerFactory.getLogger(Provider.class);
+	
 	public static void main(String[] args) throws IOException {
+		logger.info("test log!");
+		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"dubbo/applicationContext-provider.xml"});  
         context.start();  
         System.in.read(); // 为保证服务一直开着，利用输入流的阻塞来模拟 
