65c1ad6501fd799b778fda76c569d822d080f6dd
peng.zhang <figerzpeng@163.com>
2016-02-18 11:19:37 +0800

    test spring resource FileSystemResource and ClassPathResource to access file reousrce

---
+++ spring3x-test/src/test/java/com/figer/spring/resource/SpringResourceTest.java
@@ -0,0 +1,27 @@
+package com.figer.spring.resource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+public class SpringResourceTest {
+	public static void main(String[] args) throws IOException {
+		String filePath = "/Users/figer/Pictures/luffy.jpeg";
+		Resource fileResource = new FileSystemResource(filePath);
+		InputStream luffyStream = fileResource.getInputStream();
+		System.out.println(fileResource.getFilename());
+		System.out.println(fileResource.getDescription());
+		System.out.println(fileResource.getURL());
+		System.out.println(fileResource.getURI());
+		
+		System.out.println(luffyStream.read());
+		luffyStream.close();
+		
+		Resource classpathResource = new ClassPathResource("applicatioin.xml");
+		System.out.println(classpathResource.getFilename());
+		
+	}
+}
