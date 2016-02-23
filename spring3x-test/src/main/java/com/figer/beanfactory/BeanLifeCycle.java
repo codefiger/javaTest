5bedad3a89de01553eb4cb79db090e8cbcaa42dd
peng.zhang <figerzpeng@163.com>
2016-02-23 11:23:28 +0800

    spring test , applicationcontext 和 bean factory 异同

--- spring3x-test/src/main/java/com/figer/beanfactory/BeanLifeCycle.java
+++ spring3x-test/src/main/java/com/figer/beanfactory/BeanLifeCycle.java
@@ -3,11 +3,14 @@ package com.figer.beanfactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
 import com.figer.domain.Car;
+import com.figer.domain.User;
 
 public class BeanLifeCycle {
 	private static void lifeCycleInBeanFactory(){
@@ -23,6 +26,13 @@ public class BeanLifeCycle {
 		Car car2 = beanFactory.getBean("car",Car.class);
 		System.out.println(car == car2);
 		((XmlBeanFactory)beanFactory).destroySingletons();
+		
+		//application 和 beanFactory 最大的区别就是前者会利用Java反射自动识别出配置定义的BeanPostProcessor,InstantiationAwareBeanPostProcessor
+		//但是bean类就耦合了spring的BeanPostProcessor,InstantiationAwareBeanPostProcessor接口，如何解耦？
+		//init-method,destroy-method 可以实现解耦并对bean进行控制
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:beans.xml");
+		Car car3 = applicationContext.getBean("car", Car.class);
+		System.out.println(car3);
 	}
 	
 	public static void main(String[] args) {
