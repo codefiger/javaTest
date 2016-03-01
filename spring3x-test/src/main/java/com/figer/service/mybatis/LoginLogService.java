5f0839f8c1324ed0e1e0cb61856956ef75906573
peng.zhang <figerzpeng@163.com>
2016-03-01 15:28:56 +0800

    spring Transaction test,isolation隔离级别

--- spring3x-test/src/main/java/com/figer/service/mybatis/LoginLogService.java
+++ spring3x-test/src/main/java/com/figer/service/mybatis/LoginLogService.java
@@ -2,6 +2,8 @@ package com.figer.service.mybatis;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.figer.gen.mybatis.TLoginLog;
 import com.figer.gen.mybatis.TLoginLogExample;
@@ -25,4 +27,31 @@ public class LoginLogService {
 	public boolean updateLoginLog(TLoginLog log){
 		return tLoginLogMapper.updateByPrimaryKey(log) >= 1;
 	}
+	
+	public void loginLogTransaction(String ip){
+		TLoginLog log = getLoginLogByIp("test-1");
+		System.out.println(log.getUserId());
+		log.setUserId(log.getUserId() + 1);
+		
+		
+		updateLoginLog(log);
+		System.out.println("----------success");
+		throw new RuntimeException();
+	}
+	
+	public void loginLogTransaction2(String ip){
+		TLoginLog log = getLoginLogByIp("test-1");
+		System.out.println(log.getUserId());
+		log.setUserId(log.getUserId() + 1);
+		
+		try {
+			Thread.sleep(15000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		updateLoginLog(log);
+	}
+	
+	
 }
--- spring3x-test/src/test/java/com/figer/service/LoginLogServiceTest.java
+++ spring3x-test/src/test/java/com/figer/service/LoginLogServiceTest.java
@@ -5,21 +5,31 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.figer.gen.mybatis.TLoginLog;
 import com.figer.service.mybatis.LoginLogService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={
 		"classpath*:applicationContext-mybatis.xml"
 })
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = false) 
 public class LoginLogServiceTest {
 	@Autowired
 	private LoginLogService loginLogService;
 	
 	@Test
+	@Transactional(isolation=Isolation.SERIALIZABLE)
 	public void testTransactionRepeatabeRead(){
-		TLoginLog log = loginLogService.getLoginLogByIp("test-1");
-		System.out.println(log.getLoginLogId());
+		loginLogService.loginLogTransaction("test-1");
 	}
+	
+	@Test
+	@Transactional(isolation=Isolation.SERIALIZABLE)
+	public void testTransactionRepeatabeRead2(){
+		loginLogService.loginLogTransaction2("test-1");
+	}
+	
 }
