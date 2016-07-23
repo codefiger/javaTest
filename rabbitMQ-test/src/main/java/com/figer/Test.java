8ec767b84f58a8f60293774cc2bf84871fb89d37
figer <figerzpeng@gmail.com>
2016-07-23 20:41:22 +0800

    CompletionService test

--- rabbitMQ-test/src/main/java/com/figer/Test.java
+++ rabbitMQ-test/src/main/java/com/figer/Test.java
@@ -1,17 +1,59 @@
 package com.figer;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Test{
-  public static void main(String[] args) {
-    System.out.println("111");
+  public static void main(String[] args) throws Exception{
 
-    BigDecimal bigDecimal = new BigDecimal(2.111);
-    BigDecimal b = new BigDecimal(3.2);
-    System.out.println(bigDecimal.multiply(b).setScale(2, RoundingMode.HALF_UP));
+    ExecutorService exec = Executors.newCachedThreadPool();
+    CompletionService<Integer> completionService = new ExecutorCompletionService<Integer>(exec);
+    int[] index = {1,2,3,4,5,6,7,8};
+    for(final int i : index){
+      try {
+        completionService.submit(new Callable<Integer>() {
+          @Override
+          public Integer call() throws Exception {
+            System.out.println("...." + i);
+            Thread.sleep(1500);
+            if(i == 4)
+              throw new RuntimeException("111");
+            return new Random().nextInt(10);
+          }
+        });
+      }catch (Exception e){
+        System.out.println("ex");
+      }
 
-    System.out.println(new BigDecimal(0.0100).compareTo(new BigDecimal(0.01)) == -1);
+    }
+    int sum = 0;
+    for(int i=0; i<index.length; i++){
+      try {
+        completionService.take().get(2, TimeUnit.SECONDS);
+        sum ++;
+      }catch (Exception e){
+        System.out.println("ex" + i);
+      }
 
+    }
+    System.out.println("总数为："+sum);
+  }
+}
+
+class View{
+  private boolean showErrorMsg;
+
+  public boolean isShowErrorMsg() {
+    return showErrorMsg;
+  }
+
+  public View setShowErrorMsg(boolean showErrorMsg) {
+    this.showErrorMsg = showErrorMsg;
+    return this;
   }
 }
