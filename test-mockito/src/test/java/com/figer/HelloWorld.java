5c716fa37b6793c2e0343630fcb66992f708b632
figer <figerzpeng@gmail.com>
2016-07-24 09:16:05 +0800

    test-mockito:hello world

---
+++ test-mockito/src/test/java/com/figer/HelloWorld.java
@@ -0,0 +1,44 @@
+package com.figer;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+/**
+ * Created by figer on 7/24/16.
+ */
+public class HelloWorld {
+
+  @Test
+  public void testVerify(){
+    System.out.println("testPortfolioService start");
+
+    //mock creation
+    List mockedList = mock(List.class);
+
+    //using mock object - it does not throw "unexpected interaction" exception
+    mockedList.add("one");
+    //mockedList.clear();
+
+    //selective, explicit, highly readable verification
+    verify(mockedList, atLeastOnce()).add("one");
+    verify(mockedList).clear();
+  }
+
+  @Test
+  public void testStubMethod(){
+    //you can mock concrete classes, not only interfaces
+    LinkedList mockedLinkedList = mock(LinkedList.class);
+
+    //stubbing appears before the actual execution
+    when(mockedLinkedList.get(0)).thenReturn("first");
+
+    //the following prints "first"
+    System.out.println(mockedLinkedList.get(0));
+
+    //the following prints null because get(999) was not stubbed
+    System.out.println(mockedLinkedList.get(999));
+  }
+}
