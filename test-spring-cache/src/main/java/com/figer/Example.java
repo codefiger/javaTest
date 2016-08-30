5f1fa40bd28ce81760ff5342ef051b0eebf9d579
figer <figerzpeng@gmail.com>
2016-08-30 22:51:53 +0800

    example:init

---
+++ test-spring-cache/src/main/java/com/figer/Example.java
@@ -0,0 +1,16 @@
+package com.figer;
+
+/**
+ * Created by figer on 8/30/16.
+ */
+public class Example {
+  public static void main(String[] args) {
+    // 1 build plane
+
+    // 2 init plane
+
+    // 3 play plane...
+
+    // 4 collect plane
+  }
+}
