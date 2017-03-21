63bb6f81ed2366f3a796b5bb10eb94ddb0a5419b
figer <figerzpeng@gmail.com>
2017-03-21 22:56:43 +0800

    algs4: 1.3 Implement Bag with LinkedList

--- javase-test/src/main/java/com/figer/arithmetic/algs4/collections/api/IBag.java
+++ javase-test/src/main/java/com/figer/arithmetic/algs4/collections/api/IBag.java
@@ -3,5 +3,7 @@ package com.figer.arithmetic.algs4.collections.api;
 /**
  * Created by figer on 21/03/2017.
  */
-public interface IBag {
+public interface IBag<T>{
+  void add(T t);
+  boolean isEmpty();
 }
---
+++ javase-test/src/main/java/com/figer/arithmetic/algs4/test/CollectionTest.java
@@ -0,0 +1,26 @@
+package com.figer.arithmetic.algs4.test;
+
+
+import com.figer.arithmetic.algs4.collections.Bag;
+import org.junit.Test;
+
+/**
+ * Created by figer on 21/03/2017.
+ */
+public class CollectionTest {
+  @Test
+  public void bagTest(){
+    Bag<String> nums = new Bag<>();
+    String inputStr = "1 2 3 4 5 6";
+    String inputList[] = inputStr.split(" ");
+    for (int i = 0; i < inputList.length; i++) {
+      nums.add(inputList[i]);
+    }
+
+    print(nums);
+  }
+
+  private void print(Iterable iterable){
+    iterable.forEach(obj -> System.out.println(obj));
+  }
+}
